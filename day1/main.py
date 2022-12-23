160fbe9b84d825edf55654c597039f4ec1fb6a3d
Adam Andersson <adama-98@hotmail.com>
2022-12-23 13:12:09 +0100

    Day 1 - star two

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,24 @@
 with open('input1.txt', 'r') as f:
     input_data = f.readlines()
 
-best = 0
+best = [0, 0, 0]
 curr = 0
 for item in input_data:
     if item == '\n':
+        if curr > best[2]:
+            best[0] = best[1]
+            best[1] = best[2]
+            best[2] = curr
+        elif curr > best[1]:
+            best[0] = best[1]
+            best[1] = curr
+        elif curr > best[0]:
+            best[0] = curr
+
         curr = 0
         continue
     else:
         num = int(item)
         curr += num
 
-    if curr > best:
-        best = curr
-
-print(best)
+print(sum(best))
